518bd750d4c6d008362e4b94bf2dfcb3a63d5bef
Aravind Gokaraju <aravindgokaraju8@gmail.com>
2025-09-03 15:44:53 -0500

    debug attempt 1

--- PyMonitor/wsgi.py
+++ PyMonitor/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PyMonitor.settings")
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PyMonitor.settings.production")
+print("Change Actually showing through github")
 application = get_wsgi_application()
