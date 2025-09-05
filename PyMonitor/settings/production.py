d1ee6835e40e3e4e24e6b843693727a1bf1cc133
Aravind Gokaraju <aravindgokaraju8@gmail.com>
2025-09-05 13:43:29 -0500

    refresh

--- PyMonitor/settings/production.py
+++ PyMonitor/settings/production.py
@@ -25,21 +25,21 @@ if os.environ.get('SUPA_URI'):
             ssl_require=True
         )
     }
-else:
-    # Fallback to MySQL
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': os.environ.get('DB_NAME'),
-            'USER': os.environ.get('DB_USER'),
-            'PASSWORD': os.environ.get('DB_PASSWORD'),
-            'HOST': os.environ.get('DB_HOST'),
-            'PORT': os.environ.get('DB_PORT'),
-            'OPTIONS': {
-                'sql_mode': 'STRICT_TRANS_TABLES',
-            }
-        }
-    }
+# else:
+#     # Fallback to MySQL
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.mysql',
+#             'NAME': os.environ.get('DB_NAME'),
+#             'USER': os.environ.get('DB_USER'),
+#             'PASSWORD': os.environ.get('DB_PASSWORD'),
+#             'HOST': os.environ.get('DB_HOST'),
+#             'PORT': os.environ.get('DB_PORT'),
+#             'OPTIONS': {
+#                 'sql_mode': 'STRICT_TRANS_TABLES',
+#             }
+#         }
+#     }
 # # Database
 # DATABASES = {
 #     'default': {
